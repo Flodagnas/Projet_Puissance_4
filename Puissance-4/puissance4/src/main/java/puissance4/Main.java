43b12a4250a6e8cc3e23d14d4a7a833653ef1146
Mathis <mathis.veron@ynov.com>
2021-09-26 21:25:02 +0200

    complétion de code

--- Puissance-4/puissance4/src/main/java/puissance4/Main.java
+++ Puissance-4/puissance4/src/main/java/puissance4/Main.java
@@ -2,7 +2,7 @@ package puissance4;
 
 import java.util.Scanner;
 
-public class Puissance4 {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
